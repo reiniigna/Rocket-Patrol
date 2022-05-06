5252838c9260c295faf32b2764e88dbfe11c0238
Reini <rigna@ucsc.edu>
2022-05-06 16:15:37 -0700

    up to keeping score

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,10 +8,13 @@ class Play extends Phaser.Scene {
         this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/spaceship.png');
         this.load.image('starfield', './assets/starfield.png');
+
+        //load spritesheets
+        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
 
     create() {
-        //place tile sprite
+        // place tile sprite
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
 
         // green UI background
@@ -36,14 +39,60 @@ class Play extends Phaser.Scene {
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
+        // animation config
+        this.anims.create({
+            key: 'explode',
+            frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}), 
+            frameRate: 30
+        });
     }
 
     update() {
-        this.starfield.tilePositionX -= 4;
-        this.p1Rocket.update();
+        this.starfield.tilePositionX -= 4;  // update tile sprite
+        this.p1Rocket.update();             // update p1   
         this.ship01.update();               // update spaceships (x3)
         this.ship02.update();
         this.ship03.update();
+
+    // check collisions
+    if(this.checkCollision(this.p1Rocket, this.ship03)) {
+        this.p1Rocket.reset();
+        this.shipExplode(this.ship03);
+        }
+    if(this.checkCollision(this.p1Rocket, this.ship02)) {
+        this.p1Rocket.reset();
+        this.shipExplode(this.ship02);
+        }
+    if(this.checkCollision(this.p1Rocket, this.ship01)) {
+        this.p1Rocket.reset();
+        this.shipExplode(this.ship01);
+        }
     }
 
+    checkCollision(rocket, ship) {
+        //simple AABB checking
+        if  (rocket.x < ship.x + ship.width &&
+             rocket.x + rocket.width > ship.x &&
+             rocket.y < ship.y + ship.height &&
+             rocket.height + rocket.y > ship.y) {
+                return true;
+        } else {
+            return false;
+        }
+    }
+
+    shipExplode(ship) {
+        // temporarily hide ship
+        ship.alpha = 0; 
+        // create explosion sprite at ship's position
+        let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
+        boom.anims.play('explode');             // play explode animation
+        boom.on('animationcomplete', () => {    // callback after anim completes
+            ship.reset();                       // reset ship position
+            ship.alpha = 1;                     // make ship visible again
+            boom.destroy();                     // remove explosion sprite
+        });
+    }
+        
 }
