31b9cb4a508dcf515110de3e54a9fa288f388e45
Reini <rigna@ucsc.edu>
2022-05-06 18:01:18 -0700

    timer and sounds

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,6 +11,7 @@ class Play extends Phaser.Scene {
 
         //load spritesheets
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+
     }
 
     create() {
@@ -46,14 +47,55 @@ class Play extends Phaser.Scene {
             frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}), 
             frameRate: 30
         });
+
+        // initialize score
+        this.p1Score = 0;
+
+        //display score
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 100
+        }
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+
+        // GAME OVER flag
+        this.gameOver = false;
+
+        // 60-second play clock
+        scoreConfig.fixedWidth = 0;
+        this.clock = this.time.delayedCall(60000, () => {
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart',
+        scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
+        }, null, this);
+
     }
 
     update() {
-        this.starfield.tilePositionX -= 4;  // update tile sprite
+    
+    // check key input for restart
+    if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+        this.scene.restart();
+    }
+
+    if (!this.gameOver)  {
         this.p1Rocket.update();             // update p1   
         this.ship01.update();               // update spaceships (x3)
         this.ship02.update();
         this.ship03.update();
+    }
+    
+    this.starfield.tilePositionX -= 4;  // update tile sprite
+
 
     // check collisions
     if(this.checkCollision(this.p1Rocket, this.ship03)) {
@@ -93,6 +135,11 @@ class Play extends Phaser.Scene {
             ship.alpha = 1;                     // make ship visible again
             boom.destroy();                     // remove explosion sprite
         });
+        // score add and repaint
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score;
+
+        this.sound.play('sfx_explosion');
     }
         
 }
